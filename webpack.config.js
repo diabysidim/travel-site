33ca437e27510ce176ff514b78b750651e858169
Sidi <diabysidim@gmail.com>
2020-10-15 09:50:27 -0500

    adding webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path= require("path");
+const postCSSPlugin=[
+    require("postcss-simple-vars"),
+    require("postcss-nested"),
+    require("autoprefixer")
+]
+module.exports= {
+    entry: './app/assets/scripts/App.js',
+    output:{
+        filename:"bundle.js",
+        path: path.resolve(__dirname, 'app')
+    },
+    mode:"development", 
+    watch:true,
+    module:{
+
+        rules: [
+            {
+                test: /\.css$/i,
+                use:['style-loader','css-loader?url=false', {loader:'postcss-loader', options:{ postcssOptions: { plugins: postCSSPlugin} }}]
+            }
+        ]
+    }
+}
